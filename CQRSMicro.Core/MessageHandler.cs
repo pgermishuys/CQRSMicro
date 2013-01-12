7ba22fafbcbc8fc40ae095e552dc7b6c8d4587f8
pgermishuys <pieter@pieterg.com>
2013-01-12 10:06:55 +0200

    Simplified the Abstract Domain Service to inherit from a generic Message Handler that knows how to handle messages. Added an Abstract Process for demonstration purposes

---
+++ CQRSMicro.Core/MessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRSMicro.Core
+{
+	public class MessageHandler : IMessageHandler
+	{
+		public virtual void Handle(IMessage message)
+		{
+			((dynamic)this).When((dynamic)message);
+		}
+	}
+}
